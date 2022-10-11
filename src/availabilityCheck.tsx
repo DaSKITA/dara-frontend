bf467448d52e838b389d28c922a01043f211001c
flow <git@lowspace.de>
2022-10-11 10:47:02 +0200

    Add extension icons

--- src/availabilityCheck.tsx
+++ src/availabilityCheck.tsx
@@ -4,6 +4,8 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useSnackbar } from 'notistack';
+import { ReactComponent as ChromeLogo } from './assets/chrome-webstore.svg';
+import { Container } from '@mui/system';
 
 
 export const checkExtensionAvailability = () => {
@@ -15,9 +17,8 @@ export const checkExtensionAvailability = () => {
 }
 
 export default function ExtensionAvailabilityCheck() {
-    const { enqueueSnackbar } = useSnackbar();
     const [open, setOpen] = React.useState(false);
-
+    const { enqueueSnackbar } = useSnackbar();
     const handleClose = (event: any, reason: any) => {
         if (reason && reason === "backdropClick")
             return;
@@ -26,7 +27,7 @@ export default function ExtensionAvailabilityCheck() {
 
     React.useEffect(() => {
         if (checkExtensionAvailability()) {
-            enqueueSnackbar(`Browser extension is available, ready to go!`)
+            enqueueSnackbar(`Connected to browser extension, ready to go!`)
         } else {
             setOpen(true);
         }
@@ -49,17 +50,30 @@ export default function ExtensionAvailabilityCheck() {
                     </DialogContentText>
                     <br></br>
                     <DialogContentText id="alert-dialog-description" sx={{ 'text-align': 'center' }} >
-                        You can download it here:
+                        You can download it for chrome-based browsers or firefox:
                     </DialogContentText>
+                    <br></br>
+                    <Container sx={{ 'align': 'center' }}>
+                        <table>
+                            <tr>
+                                <td>
+                                    <Container sx={{ 'width': '15rem' }}>
+                                        <a href="https://chrome.google.com/webstore/detail/automa/heolgaalbnnelipfhbccbkdohecmaimo">
+                                            <ChromeLogo />
+                                        </a>
+                                    </Container>
+                                </td>
+                                <td>
+                                    <Container sx={{ 'width': '15rem' }}>
+                                        <a href="https://addons.mozilla.org/en-US/firefox/addon/dara/">
+                                            <img src="https://user-images.githubusercontent.com/22908993/166417727-3481fef4-00e5-4cf0-bb03-27fb880d993c.png" alt="Firefox add-ons" />
+                                        </a>
+                                    </Container>
+                                </td>
+                            </tr>
+                        </table>
+                    </Container>
                 </DialogContent>
-                {/*
-                <DialogActions>
-                    <Button onClick={handleClose}>Disagree</Button>
-                    <Button onClick={handleClose} autoFocus>
-                        Agree
-                    </Button>
-                </DialogActions>
-                */}
             </Dialog>
         </>
     );
